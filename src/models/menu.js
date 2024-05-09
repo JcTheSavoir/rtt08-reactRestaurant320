319a55ff27a330ad0a09ea923ccb7b157a41185a
JcTheSavior <jacobconnell3@gmail.com>
2024-05-09 00:01:02 -0400

    Add more testing data to menu

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,18 +1,25 @@
 const menu = [
     {
-        type: "desserts",
+        type: "Desserts",
         items:[
             {
-                name:"Chocolate Lasagna", 
+                name: "Chocolate Lasagna", 
                 calories: "980 cal",
                 imageURL: "https://media.olivegarden.com/en_us/images/marketing/Chocolate_Lasagna_590X365.jpg",
                 description: "Decadent layers of chocolate cake, creamy chocolate mousse, and a delicate layer of crushed wafers. Topped with chocolate sauce.",
                 price: "$9.29"
+            },
+            {
+                name: "Sicilian Cheesecake",
+                calories: "700 cal",
+                imageURL: "https://media.olivegarden.com/en_us/images/marketing/Strawberry-Cheesecake-gv-1180x730.jpg",
+                description: "Ricotta cheesecake with a shortbread cookie crust, topped with strawberry sauce.",
+                price: "$8.99",         
             }
         ]
     },
     {
-        type: "entree",
+        type: "Entrees",
         items:[
             {
                 name: "Lasagna Classico",
@@ -21,11 +28,18 @@ const menu = [
                 description: `Prepared fresh daily with layers of pasta, parmesan, mozzarella, pecorino romano and our homemade meat sauce.**
                 **Our meat sauce is made with pan-seared beef and Italian sausage.`,
                 price: "$17.79"
+            },
+            {
+                name: "Chicken Parmigiana",
+                calories: "1,020 cal",
+                imageURL: "https://media.olivegarden.com/en_us/images/product/dinner-chicken-parm-dpv-590x365.jpg",
+                description: "Two lightly fried parmesan-breaded chicken breasts with our homemade marinara and melted Italian cheeses. Served with a side of spaghetti.",
+                price: "$19.49",
             }
         ]
     },
     {
-        type: "drinks",
+        type: "Drinks",
         items:[
             {
                 name: "Lavazza Espresso",
@@ -33,6 +47,13 @@ const menu = [
                 imageURL: "https://media.olivegarden.com/en_us/images/marketing/Espresso-dpv-1180x730.jpg",
                 description: "Italy's favorite coffee",
                 price: "$3.99",
+            },
+            {
+                name: "Cappuccino",
+                calories: "150 cal",
+                imageURL: "https://media.olivegarden.com/en_us/images/product/d-cappuccino-with-whipped-cream-dpv.jpg",
+                description: "Lavazza Espresso and steamed milk, topped with whipped cream and a sprinkle of cinnamon.",
+                price: "$4.99",
             }
         ]
     }
